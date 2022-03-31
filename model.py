e6e290592b3a2694f49baf286eeef8d667642cf8
Darius Kianersi <dariuskianersi@gmail.com>
2022-03-30 22:24:09 -0400

    added dna and reuters datasets

---
+++ model.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+
+chess_df = pd.read_csv("datasets/chess/kr-vs-kp.data")
+# led_df = pd.read_csv("datasets/led/led-creator")
+votes_df = pd.read_csv("datasets/votes/house-votes-84.data")
